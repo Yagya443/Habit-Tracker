e95e6ed7f00b027f140b8b704243e8c67664a971
yagya <yagnaakv99@gmail.com>
2026-05-17 11:00:53 +0530

    User model

--- BACKEND/src/models/User.model.js
+++ BACKEND/src/models/User.model.js
@@ -1,9 +1,28 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    
-
-
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            required: true,
+            type: String,
+        },
+        email: {
+            required: true,
+            type: String,
+            unique: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 8,
+        },
+        avatar: {
+            type: String,
+            default: "",
+        },
+    },
+    { timestamps: true },
+);
 
 module.exports = mongoose.model("User", userSchema);
